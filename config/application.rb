68d8f9d87c691b1a8aec9e85a56243a8c8ad85ba
i5okie <ipzent@gmail.com>
2014-01-02 16:14:41 -0800

    tiny cleanup

--- config/application.rb
+++ config/application.rb
@@ -3,7 +3,6 @@ require File.expand_path('../boot', __FILE__)
 #require 'rails/all'
 
 require "action_controller/railtie"
-require "active_resource/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
 
