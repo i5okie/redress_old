10f92d8e00659f50ad741e2ba0d26417958edd11
i5okie <ipzent@gmail.com>
2014-01-14 11:53:48 -0800

    new model

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,5 +1,8 @@
 class Document
   include Mongoid::Document
+  embedded_in :guides, polymorphic: true
+
+
   field :name, type: String
   field :description, type: String
   field :filetype, type: String
